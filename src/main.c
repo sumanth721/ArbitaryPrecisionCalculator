fe9407dba1533a29fa8b59ee3b593e2b69f0693e
sumanth <sumanth.sarvepalli@gmail.com>
2022-04-08 11:28:44 +0530

    Initial Commit

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ *
+ * Title        : Arbitary Precision Calculator
+ * Author       : Sumanth
+ * Description  : This Project is to develop a Precision Calculator
+ *                
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to APC Project\n");
+
+    return 0;
+}
